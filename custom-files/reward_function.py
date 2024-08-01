6edd87364e1a5be19d0a17607084ecafab14b6c9
Tejesh <mandalatejesh01@gmail.com>
2024-08-01 17:45:14 +0530

    update reward f

--- custom-files/reward_function.py
+++ custom-files/reward_function.py
@@ -60,28 +60,8 @@ Good:
 
     
     '''
-    
-    # Read input parameters
-    distance_from_center = params['distance_from_center']
-    track_width = params['track_width']
-    steering = abs(params['steering_angle']) # Only need the absolute steering angle
-
-    # Calculate 3 marks that are farther and father away from the center line
-    marker_1 = 0.15 * track_width
-    marker_2 = 0.25 * track_width
-    marker_3 = 0.5 * track_width
-
+    reward = 0
     speed = params['speed']    
-    max_speed = 3
-    # Give higher reward if the car is closer to center line and vice versa
-    if distance_from_center <= marker_1:
-        reward = 1
-    elif distance_from_center <= marker_2:
-        reward = 0.5
-    elif distance_from_center <= marker_3:
-        reward = 0.1
-    else:
-        reward = 1e-3  # likely crashed/ close to off track
     
     waypoints = params['waypoints']
     closest_waypoints = params['closest_waypoints']
@@ -114,7 +94,7 @@ Good:
         if speed < 0.7:
             reward += 0.2
     elif (abs(speed - desired_speed) < 0.3):
-        reward += 0.3
+        reward += 0.5
         
     if not (should_be_left ^ params['is_left_of_center']):
         reward += 0.3
